3866dde68b02a78f380e827632625df4d7475a07
xela85 <alex.3385@hotmail.fr>
2017-02-23 13:42:52 +0100

    Initial commit

---
+++ app/src/main/java/fr/imt/lebrun/BooksActivity.java
@@ -0,0 +1,13 @@
+package fr.imt.lebrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+    }
+}
